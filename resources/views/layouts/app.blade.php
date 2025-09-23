059d8768ad362cf81acf16b85da102d0c9ae6591
Irham Faridz Rizky <irhamfrdz@gmail.com>
2025-09-23 14:30:48 +0700

    fix: shorten Pembayaran Tagihan Kontainer dropdown text
    - Changed dropdown title from 'Pembayaran Tagihan Kontainer' to 'Bayar Tagihan Kontainer'
    - Shortened menu items:
      - 'Pembayaran Pranota Tagihan Kontainer Sewa'  'Bayar Pranota Kontainer Sewa'
      - 'Pembayaran Pranota Perbaikan Kontainer'  'Bayar Pranota Perbaikan Kontainer'
    - Improved sidebar space utilization and readability
    - Maintained clear meaning while reducing text length

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -893,7 +893,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"/>
                 </svg>
             </div>
-            <span class="text-xs font-medium truncate w-full">Pembayaran Tagihan Kontainer</span>
+            <span class="text-xs font-medium truncate w-full">Bayar Tagihan Kontainer</span>
         </div>
         <svg class="w-4 h-4 transition-transform duration-200 dropdown-arrow {{ $isPembayaranTagihanKontainerRoute ? 'rotate-180' : '' }}" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/>
@@ -906,7 +906,7 @@
                 <svg class="w-3 h-3 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"/>
                 </svg>
-                Pembayaran Pranota Tagihan Kontainer Sewa
+                Bayar Pranota Kontainer Sewa
             </a>
         @endif
 
@@ -917,7 +917,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"/>
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
                 </svg>
-                Pembayaran Pranota Perbaikan Kontainer
+                Bayar Pranota Perbaikan Kontainer
             </a>
         @endif
     </div>
