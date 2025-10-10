b4475c97fef5ca0f92bd5c7ab034f69976f25281
Irham Faridz Rizky <irhamfrdz@gmail.com>
2025-10-10 10:08:30 +0700

    Fix MasterKegiatan model - Add 'type' to fillable array
    - Added 'type' field to fillable array in MasterKegiatan model
    - This fixes the issue where type field cannot be saved to database
    - Migration for type column was already run, only missing fillable declaration
    
    Fixes: Type field not being saved when creating master kegiatan

--- app/Models/MasterKegiatan.php
+++ app/Models/MasterKegiatan.php
@@ -14,6 +14,7 @@ class MasterKegiatan extends Model
     protected $fillable = [
         'kode_kegiatan',
         'nama_kegiatan',
+        'type',
         'keterangan',
         'status',
     ];
