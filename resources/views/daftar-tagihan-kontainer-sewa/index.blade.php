bd0e35e41e3db61fed585100b087b469daf78e3b
irhamfrdz <irhamfrdz@gmail.com>
2025-11-16 18:46:10 -0800

    Reduce left and right padding to make table more compact and utilize space better

--- resources/views/daftar-tagihan-kontainer-sewa/index.blade.php
+++ resources/views/daftar-tagihan-kontainer-sewa/index.blade.php
@@ -188,7 +188,7 @@ input[required]:focus {
 }
 </style>
 
-<div class="container mx-auto p-4">
+<div class="container mx-auto p-1">
     <!-- Notification Container -->
     <div id="notification-container" class="notification-container"></div>
     <h1 class="text-2xl font-bold mb-4">Daftar Tagihan Kontainer Sewa</h1>
@@ -579,7 +579,7 @@ input[required]:focus {
     </div>
     @endif
 
-    <div class="max-w-full mx-auto px-4">
+    <div class="max-w-full mx-auto px-1">
         <div class="bg-white rounded-lg shadow-sm border overflow-hidden">
             <!-- Rows per page control -->
             <div class="px-4 py-2 border-b border-gray-200">
