9508984115e001f779774af77ef295e0cef47727
irhamfrdz <irhamfrdz@gmail.com>
2025-12-04 14:36:21 +0700

    Allow empty nomor tanda terima input in LCL form
    - Updated placeholder to clarify that field can be left empty
    - Changed help text to indicate field is optional
    - Improved user experience for LCL tanda terima creation

--- resources/views/tanda-terima-tanpa-surat-jalan/create-lcl.blade.php
+++ resources/views/tanda-terima-tanpa-surat-jalan/create-lcl.blade.php
@@ -147,10 +147,10 @@
                             <input type="text" name="nomor_tanda_terima" id="nomor_tanda_terima"
                                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
                                    value="{{ old('nomor_tanda_terima') }}"
-                                   placeholder="TTR-LCL-001 (kosongkan jika input belakangan)">
+                                   placeholder="TTR-LCL-001 (boleh dikosongkan)">
                             <p class="mt-1 text-xs text-gray-500">
                                 <i class="fas fa-info-circle mr-1"></i>
-                                Input manual, boleh diisi belakangan
+                                Input manual, boleh dikosongkan jika belum ada nomor
                             </p>
                             @error('nomor_tanda_terima')
                                 <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
