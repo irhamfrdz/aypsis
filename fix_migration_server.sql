50440a690fc109f4b14c390b8988a0f23fbd90f7
irhamfrdz <irhamfrdz@gmail.com>
2025-12-15 15:59:15 +0700

    Include all tanda_terima_lcl related migrations in fix script

--- fix_migration_server.sql
+++ fix_migration_server.sql
@@ -18,8 +18,21 @@ ORDER BY batch DESC, migration;
 SET @next_batch = (SELECT MAX(batch) + 1 FROM migrations);
 
 INSERT INTO migrations (migration, batch) VALUES 
+('2025_10_25_124630_create_tanda_terima_lcl_table', @next_batch),
+('2025_10_25_124657_create_tanda_terima_lcl_items_table', @next_batch),
+('2025_10_27_103613_fix_tanda_terima_lcl_tujuan_pengiriman_foreign_key', @next_batch),
+('2025_10_27_130917_add_nomor_kontainer_to_tanda_terima_lcl_table', @next_batch),
+('2025_10_27_131639_add_size_kontainer_to_tanda_terima_lcl_table', @next_batch),
+('2025_10_27_131957_add_seal_columns_to_tanda_terima_lcl_table', @next_batch),
+('2025_10_27_150123_fix_size_kontainer_enum_values_in_tanda_terima_lcl_table', @next_batch),
+('2025_12_01_153953_drop_jenis_barang_id_from_tanda_terima_lcl_table', @next_batch),
+('2025_12_01_155316_add_item_details_to_tanda_terima_lcl_items_table', @next_batch),
+('2025_12_04_143703_make_nomor_tanda_terima_nullable_in_tanda_terima_lcl_table', @next_batch),
+('2025_12_04_160829_add_gambar_surat_jalan_to_tanda_terima_lcl_table', @next_batch),
+('2025_12_04_161933_add_jenis_kontainer_to_tanda_terima_lcl_table', @next_batch),
 ('2025_12_15_090852_create_tanda_terima_lcl_tables', @next_batch),
 ('2025_12_15_091741_remove_unused_columns_from_tanda_terimas_lcl_table', @next_batch),
+('2025_12_15_092519_add_item_number_and_fix_columns_to_tanda_terima_lcl_items_table', @next_batch),
 ('2025_12_15_144001_drop_unused_pivot_tables_tanda_terima_lcl', @next_batch),
 ('2025_12_15_144324_add_container_fields_to_tanda_terimas_lcl_table', @next_batch),
 ('2025_12_15_144932_create_tanda_terima_lcl_kontainer_pivot_table', @next_batch),
