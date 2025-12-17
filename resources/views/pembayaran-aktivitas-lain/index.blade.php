48de93ad871174e654a74d932c4f2618d1281fac
irhamfrdz <irhamfrdz@gmail.com>
2025-12-17 12:04:06 +0700

    Add: Tambah ikon printer pada tombol print di kolom aksi pembayaran aktivitas lain

--- resources/views/pembayaran-aktivitas-lain/index.blade.php
+++ resources/views/pembayaran-aktivitas-lain/index.blade.php
@@ -121,7 +121,11 @@
                                         </a>
                                     @endcan
                                     @can('pembayaran-aktivitas-lain-print')
-                                        <a href="{{ route('pembayaran-aktivitas-lain.print', $item) }}" class="text-green-600 hover:text-green-900" target="_blank">Print</a>
+                                        <a href="{{ route('pembayaran-aktivitas-lain.print', $item) }}" class="text-green-600 hover:text-green-900" title="Print" target="_blank">
+                                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"/>
+                                            </svg>
+                                        </a>
                                     @endcan
                                     @can('pembayaran-aktivitas-lain-delete')
                                         <button onclick="confirmDelete({{ $item->id }})" class="text-red-600 hover:text-red-900" title="Hapus">
