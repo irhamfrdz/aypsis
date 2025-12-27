776f7b84e6a2fa61a955b99ec24910c0516ffc13
irhamfrdz <irhamfrdz@gmail.com>
2025-12-27 09:59:07 +0700

    Remove readonly from volume field in split form to allow submission

--- resources/views/tanda-terima-lcl/stuffing.blade.php
+++ resources/views/tanda-terima-lcl/stuffing.blade.php
@@ -810,8 +810,8 @@
                                     Volume (m³) <span class="text-red-500">*</span>
                                     <span class="text-xs text-gray-500">(otomatis dihitung)</span>
                                 </label>
-                                <input type="number" name="volume" id="split_volume" required min="0" step="0.001" readonly
-                                       class="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100 focus:outline-none"
+                                <input type="number" name="volume" id="split_volume" required min="0" step="0.001"
+                                       class="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-50 focus:outline-none focus:ring-2 focus:ring-purple-500"
                                        placeholder="0.000">
                             </div>
 
