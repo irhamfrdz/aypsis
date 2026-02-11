3015e46888581de1f289ef2027487ad6c578c1e8
irhamfrdz <irhamfrdz@gmail.com>
2026-02-11 16:23:17 +0700

    fix: format date for tanda terima bongkaran input

--- resources/views/tanda-terima-bongkaran/edit.blade.php
+++ resources/views/tanda-terima-bongkaran/edit.blade.php
@@ -57,7 +57,7 @@
                                 <input type="date" 
                                        name="tanggal_tanda_terima" 
                                        id="tanggal_tanda_terima" 
-                                       value="{{ old('tanggal_tanda_terima', $tandaTerimaBongkaran->tanggal_tanda_terima) }}"
+                                       value="{{ old('tanggal_tanda_terima', $tandaTerimaBongkaran->tanggal_tanda_terima ? $tandaTerimaBongkaran->tanggal_tanda_terima->format('Y-m-d') : '') }}"
                                        class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-teal-500 focus:border-teal-500 @error('tanggal_tanda_terima') border-red-500 @enderror"
                                        required>
                                 @error('tanggal_tanda_terima')
