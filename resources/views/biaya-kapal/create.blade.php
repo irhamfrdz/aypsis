7d38e7ac859b09653c325f787ed6e952f0b43830
irhamfrdz <irhamfrdz@gmail.com>
2026-02-11 15:15:56 +0700

    Allow manual edit for trucking subtotal
    - Remove readonly attribute from subtotal input in biaya-kapal/create.blade.php
    
    - Add input event listener to recalculate PPh and Total when subtotal is manually edited
    
    - Ensure consistent currency formatting for manual inputs

--- resources/views/biaya-kapal/create.blade.php
+++ resources/views/biaya-kapal/create.blade.php
@@ -4270,7 +4270,7 @@
                         <span class="absolute left-3 top-2.5 text-gray-400">Rp</span>
                         <input type="text" name="trucking_sections[${sectionIndex}][subtotal]" 
                                class="trucking-subtotal-input w-full pl-10 pr-3 py-2 border border-gray-300 rounded-lg bg-gray-50 focus:ring-0" 
-                               value="0" readonly>
+                               value="0">
                     </div>
                 </div>
                 <div>
@@ -4312,6 +4312,31 @@
 
         vendorSelect.addEventListener('change', function() {
             calculateTruckingTotals(sectionIndex);
+        });
+
+        // Event listener for manual subtotal edit
+        const subtotalInput = section.querySelector('.trucking-subtotal-input');
+        subtotalInput.addEventListener('input', function(e) {
+            // Remove non-numeric chars except for standard control keys
+            let rawValue = this.value.replace(/[^0-9]/g, '');
+            const numericValue = parseFloat(rawValue) || 0;
+            
+            // Format back to rupiah visually if there is a value
+            if (rawValue) {
+                this.value = new Intl.NumberFormat('id-ID').format(numericValue);
+            } else {
+                 this.value = '';
+            }
+            
+            // Recalculate PPh (2%) & Total
+            const pph = Math.round(numericValue * 0.02);
+            const total = numericValue - pph;
+            
+            section.querySelector('.trucking-pph-input').value = new Intl.NumberFormat('id-ID').format(pph);
+            section.querySelector('.trucking-total-input').value = new Intl.NumberFormat('id-ID').format(total);
+            
+            // Update Grand Total
+            calculateTotalFromAllTruckingSections();
         });
 
         // Close dropdown when clicking outside
