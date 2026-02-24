26524ac5c066c0cdd3484bc9ee2427ea08cb7884
irhamfrdz <irhamfrdz@gmail.com>
2026-02-24 10:16:38 +0700

    Add creator and updater relationships to TagihanSupirVendor model

--- app/Models/TagihanSupirVendor.php
+++ app/Models/TagihanSupirVendor.php
@@ -15,4 +15,14 @@ class TagihanSupirVendor extends Model
     {
         return $this->belongsTo(SuratJalan::class);
     }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
 }
