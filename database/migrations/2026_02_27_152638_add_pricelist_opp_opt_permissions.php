8afd5d8ff18e9b0464733babbaaed3c6212fa476
irhamfrdz <irhamfrdz@gmail.com>
2026-02-27 15:47:17 +0700

    fix permissions migration

--- database/migrations/2026_02_27_152638_add_pricelist_opp_opt_permissions.php
+++ database/migrations/2026_02_27_152638_add_pricelist_opp_opt_permissions.php
@@ -22,7 +22,6 @@ return new class extends Migration
         foreach ($permissions as $permission) {
             DB::table('permissions')->insertOrIgnore([
                 'name' => $permission,
-                'guard_name' => 'web',
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
