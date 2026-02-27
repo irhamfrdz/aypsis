b2940c199c8762b7f95c925f3aaa317c7d13ea96
irhamfrdz <irhamfrdz@gmail.com>
2026-02-27 13:38:20 +0700

    Fix namespace error in TandaTerimaLcl.php and other changes

--- app/Models/TandaTerimaLcl.php
+++ app/Models/TandaTerimaLcl.php
@@ -1,8 +1,8 @@
-    <?php
+<?php
 
-    namespace App\Models;
+namespace App\Models;
 
-    use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Relations\BelongsTo;
     use Illuminate\Database\Eloquent\Relations\HasMany;
