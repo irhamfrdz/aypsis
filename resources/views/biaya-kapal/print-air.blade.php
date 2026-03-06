174fdc8378fcd105ae49bc5bdccbd16b253a4c94
irhamfrdz <irhamfrdz@gmail.com>
2026-03-06 14:24:50 +0700

    feat: add voyage number column to print air view

--- resources/views/biaya-kapal/print-air.blade.php
+++ resources/views/biaya-kapal/print-air.blade.php
@@ -286,9 +286,10 @@
         <table class="custom-table">
             <thead>
                 <tr>
-                    <th style="width: 8%;">No</th>
-                    <th style="width: 25%;">Tanggal Ref.</th>
-                    <th style="width: 47%;">Referensi</th>
+                    <th style="width: 5%;">No</th>
+                    <th style="width: 18%;">Tanggal Ref.</th>
+                    <th style="width: 37%;">Referensi</th>
+                    <th style="width: 20%;">Nomor Voyage</th>
                     <th style="width: 20%;">Total</th>
                 </tr>
             </thead>
@@ -321,6 +322,7 @@
                         @endforeach
                         @if($references->isEmpty()) - @endif
                     </td>
+                    <td class="text-center">{{ $details->first()->voyage ?? '-' }}</td>
 
                     <td class="text-right">Rp {{ number_format($details->sum('grand_total'), 0, ',', '.') }}</td>
                 </tr>
@@ -330,7 +332,7 @@
                 </tr>
                 @endforelse
                 <tr class="total-row">
-                    <td colspan="3" class="text-right">TOTAL PEMBAYARAN</td>
+                    <td colspan="4" class="text-right">TOTAL PEMBAYARAN</td>
                     <td class="text-right">Rp {{ number_format($totalGrandTotal, 0, ',', '.') }}</td>
                 </tr>
             </tbody>
