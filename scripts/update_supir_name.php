df69d74f1611fcca4cab12123397f192aef76f21
irhamfrdz <irhamfrdz@gmail.com>
2026-03-16 13:27:09 +0700

    Update driver name script with double spaces variant support

--- scripts/update_supir_name.php
+++ scripts/update_supir_name.php
@@ -9,11 +9,31 @@ use App\Models\SuratJalan;
 echo "Memulai pembaruan nama supir...\n";
 
 // Update Supir 1
-$count1 = SuratJalan::where('supir', 'nur')->update(['supir' => 'nur cece']);
-echo "Berhasil memperbarui supir 'nur' menjadi 'nur cece' di: " . $count1 . " baris.\n";
+$variants = ['nur', 'NUR', 'nur cece', 'NUR CECE', 'Nur Cece', 'Nur', 'NUR  CECE'];
+$count1 = 0;
+$supir1Items = SuratJalan::whereIn('supir', $variants)->get();
+
+foreach ($supir1Items as $item) {
+    if ($item->supir !== 'NUR CECE') {
+        $item->supir = 'NUR CECE';
+        $item->save();
+        $count1++;
+    }
+}
+echo "Berhasil memperbarui supir variants menjadi 'NUR CECE' di: " . $count1 . " baris (force update).\n";
 
 // Update Supir 2
-$count2 = SuratJalan::where('supir2', 'nur')->update(['supir2' => 'nur cece']);
-echo "Berhasil memperbarui supir2 'nur' menjadi 'nur cece' di: " . $count2 . " baris.\n";
+$count2 = 0;
+$supir2Items = SuratJalan::whereIn('supir2', $variants)->get();
+foreach ($supir2Items as $item) {
+    if ($item->supir2 !== 'NUR CECE') {
+        $item->supir2 = 'NUR CECE';
+        $item->save();
+        $count2++;
+    }
+}
+echo "Berhasil memperbarui supir2 variants menjadi 'NUR CECE' di: " . $count2 . " baris (force update).\n";
+
+
 
 echo "Selesai.\n";
