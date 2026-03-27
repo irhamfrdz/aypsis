93f356249a7c44a5b5c98780beeab22989c6f26d
irhamfrdz <irhamfrdz@gmail.com>
2026-03-27 15:02:30 +0700

    Add SEMUA KAPAL option to Master Pricelist Labuh Tambat create and edit views

--- resources/views/master-pricelist-labuh-tambat/create.blade.php
+++ resources/views/master-pricelist-labuh-tambat/create.blade.php
@@ -54,6 +54,11 @@
                         
                         {{-- Results List --}}
                         <div id="kapal-results" class="hidden absolute z-50 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg max-h-60 overflow-y-auto">
+                            <div class="kapal-item px-4 py-2 cursor-pointer hover:bg-blue-600 hover:text-white font-bold text-blue-700" 
+                                 data-value="SEMUA KAPAL" 
+                                 data-text="SEMUA KAPAL">
+                                SEMUA KAPAL
+                            </div>
                             @foreach($kapals as $kapal)
                                 <div class="kapal-item px-4 py-2 cursor-pointer hover:bg-blue-600 hover:text-white" 
                                      data-value="{{ $kapal->nama_kapal }}" 
