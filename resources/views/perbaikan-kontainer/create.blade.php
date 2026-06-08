ea287f70939eb7f31a6a77cd397358d8a05e51fe
irhamfrdz <irhamfrdz@gmail.com>
2026-06-08 09:37:40 +0700

    feat: change vendor_cat input field to free text input instead of dropdown

--- resources/views/perbaikan-kontainer/create.blade.php
+++ resources/views/perbaikan-kontainer/create.blade.php
@@ -217,15 +217,9 @@
                                 <label for="vendor_cat" class="block text-sm font-semibold text-gray-700 mb-1">
                                     Vendor Cat <span class="text-red-500">*</span>
                                 </label>
-                                <select name="vendor_cat" id="vendor_cat"
-                                        class="w-full px-3 py-2 border border-gray-300 rounded-lg text-sm focus:ring-blue-500 focus:border-blue-500 focus:outline-none">
-                                    <option value="">-- Pilih Vendor Cat --</option>
-                                    @foreach($paintVendors as $vendor)
-                                        <option value="{{ $vendor->vendor }}" {{ old('vendor_cat') == $vendor->vendor ? 'selected' : '' }}>
-                                            {{ $vendor->vendor }}
-                                        </option>
-                                    @endforeach
-                                </select>
+                                <input type="text" name="vendor_cat" id="vendor_cat" value="{{ old('vendor_cat') }}"
+                                       placeholder="Nama vendor cat..."
+                                       class="w-full px-3 py-2 border border-gray-300 rounded-lg text-sm focus:ring-blue-500 focus:border-blue-500 focus:outline-none">
                             </div>
 
                             <!-- Jenis/Status Cat -->
