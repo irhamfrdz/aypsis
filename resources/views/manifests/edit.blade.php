d464ca299e17f8a4e51aa307e6bc172b8564e8c3
irhamfrdz <irhamfrdz@gmail.com>
2026-06-09 11:20:22 +0700

    Fix shipper input value field on manifest edit page

--- resources/views/manifests/edit.blade.php
+++ resources/views/manifests/edit.blade.php
@@ -215,7 +215,8 @@
                         <div class="relative">
                             <div class="dropdown-container-shipper">
                                 <input type="text" id="search_shipper" placeholder="Search shipper..." autocomplete="off"
-                                       class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-500 bg-white text-sm">
+                                       class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-purple-500 bg-white text-sm"
+                                       value="{{ old('pengirim', $manifest->pengirim) }}">
                                 <select name="pengirim" id="pengirim_id" class="hidden">
                                     <option value="">- Pilih Shipper -</option>
                                     @if($manifest->pengirim)
