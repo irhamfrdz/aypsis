b216d0d397ea686ffe4bdd3015cdb2710d0b03d1
irhamfrdz <irhamfrdz@gmail.com>
2026-06-17 14:57:28 +0700

    style: optimize print-spkbm spacing and margins to fit on 1 page

--- resources/views/master-kapal/print-spkbm.blade.php
+++ resources/views/master-kapal/print-spkbm.blade.php
@@ -5,12 +5,12 @@
     <title>SPKBM - {{ $validated['nomor_surat'] }}</title>
     <style>
         @page {
-            margin: 3.5cm 2cm 2cm 2cm;
+            margin: 3.5cm 2cm 1.5cm 2cm;
         }
         body {
             font-family: Arial, Helvetica, sans-serif;
-            font-size: 11pt;
-            line-height: 1.5;
+            font-size: 10pt;
+            line-height: 1.4;
             color: #000000;
         }
         .text-justify {
@@ -19,15 +19,15 @@
         .text-indent {
             text-indent: 40px;
         }
-        .mb-2 { margin-bottom: 8px; }
-        .mb-4 { margin-bottom: 16px; }
-        .mb-6 { margin-bottom: 24px; }
-        .mb-8 { margin-bottom: 32px; }
+        .mb-2 { margin-bottom: 6px; }
+        .mb-4 { margin-bottom: 12px; }
+        .mb-6 { margin-bottom: 18px; }
+        .mb-8 { margin-bottom: 24px; }
         
         .meta-table {
             width: 100%;
             border-collapse: collapse;
-            margin-bottom: 25px;
+            margin-bottom: 20px;
         }
         .meta-table td {
             vertical-align: top;
@@ -38,16 +38,17 @@
             width: 100%;
             border-collapse: collapse;
             margin-left: 40px;
-            margin-bottom: 20px;
+            margin-bottom: 15px;
         }
         .details-table td {
             vertical-align: top;
-            padding: 3px 0;
+            padding: 2px 0;
         }
         
         .signature-block {
-            margin-top: 40px;
-            line-height: 1.5;
+            margin-top: 30px;
+            line-height: 1.4;
+            page-break-inside: avoid;
         }
     </style>
 </head>
@@ -58,7 +59,7 @@
             <td style="width: 8%;">No</td>
             <td style="width: 2%;">:</td>
             <td style="width: 50%;">{{ $validated['nomor_surat'] }}</td>
-            <td style="width: 40%; text-align: right;">Jakarta, {{ \Carbon\Carbon::now()->translatedFormat('d F Y') }}</td>
+            <td style="width: 42%; text-align: right;">Jakarta, {{ \Carbon\Carbon::now()->translatedFormat('d F Y') }}</td>
         </tr>
         <tr>
             <td>Hal</td>
@@ -145,7 +146,7 @@
         Sesuai surat penunjukan kerja ini, mohon kiranya untuk penambahan jam kerja kegiatan bongkar muat mulai dari kegiatan dapat diselesaikan sampai malam/selesai.
     </div>
 
-    <div class="text-justify text-indent mb-8">
+    <div class="text-justify text-indent mb-6">
         Demikian Surat Penunjukan ini kami buat, atas perhatian dan kerjasamanya kami ucapkan terima kasih.
     </div>
 
@@ -153,7 +154,7 @@
     <div class="signature-block">
         Hormat kami,<br>
         <strong>PT. ALEXINDO YAKINPRIMA</strong>
-        <br><br><br><br><br><br>
+        <br><br><br><br>
         <strong><u>Robert</u></strong>
     </div>
 </body>
