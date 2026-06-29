50987f74ec88c84f155ab0b7bd181f209f081b60
irhamfrdz <irhamfrdz@gmail.com>
2026-06-29 15:47:05 +0700

    chore: remove temporary inspect script

--- inspect_sj.php
+++
@@ -1,7 +0,0 @@
-<?php
-$lines = file('app/Http/Controllers/ReportOngkosTrukController.php');
-foreach ($lines as $num => $line) {
-    if (strpos($line, 'InvoiceAktivitasLain::') !== false || strpos($line, '$adjInvoices') !== false) {
-        echo ($num + 1) . ": " . trim($line) . "\n";
-    }
-}
