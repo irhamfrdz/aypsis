f0a043c7897429c5fa2dffa2c4b7fc03c2dd46d6
Aditkaya <adityakaya500@gmail.com>
2026-07-24 01:49:51 +0700

    Implement auto time-based attendance state detection

--- app/Http/Controllers/Api/ADMSController.php
+++ app/Http/Controllers/Api/ADMSController.php
@@ -120,23 +120,21 @@ class ADMSController extends Controller
                     continue; // Skip format tanggal salah
                 }
                 
-                // Index ke-3 biasanya state (0=Masuk, 1=Pulang, 2=Break Out, 3=Break In, 4=OT In, 5=OT Out)
-                $state = isset($parts[3]) ? (int) $parts[3] : 0;
+                // Deteksi otomatis tipe absen berdasarkan jam (Abaikan tombol mesin)
+                $time = Carbon::parse($logTime)->format('H:i');
                 
-                if ($state == 0) {
+                if ($time >= '00:00' && $time <= '11:30') {
                     $type = 'Masuk';
-                } elseif ($state == 1) {
-                    $type = 'Pulang';
-                } elseif ($state == 2) {
+                } elseif ($time > '11:30' && $time <= '12:30') {
                     $type = 'istirahat_keluar';
-                } elseif ($state == 3) {
+                } elseif ($time > '12:30' && $time <= '13:30') {
                     $type = 'istirahat_masuk';
-                } elseif ($state == 4) {
-                    $type = 'lembur_masuk';
-                } elseif ($state == 5) {
-                    $type = 'lembur_pulang';
-                } else {
+                } elseif ($time > '13:30' && $time <= '17:30') {
                     $type = 'Pulang';
+                } elseif ($time > '17:30' && $time <= '20:00') {
+                    $type = 'lembur_masuk';
+                } else {
+                    $type = 'lembur_pulang';
                 }
 
                 // Cegah duplikasi log yang sama persis (berdasarkan NIK dan waktu spesifik)
