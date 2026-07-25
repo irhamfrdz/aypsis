7175069270f21dcb23f2a02ad57d74ac39902e4a
irhamfrdz <irhamfrdz@gmail.com>
2026-07-25 08:28:17 +0700

    Remove test_status.php

--- test_status.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$statuses = \App\Models\AlatBerat::pluck('status')->unique();
-echo "STATUSES: " . json_encode($statuses) . "\n";
-echo "COUNT: " . \App\Models\AlatBerat::count() . "\n";
