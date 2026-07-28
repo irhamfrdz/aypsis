00bb16d39a1a9210e34263226e59e91b22df37d9
irhamfrdz <irhamfrdz@gmail.com>
2026-07-28 15:09:31 +0700

    chore: remove incorrect adjustment script

--- update_adjustment_3781.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-\App\Models\StockAmprahan::where('id', 3781)->update(['adjustment' => 647885000]);
-
-echo "Adjustment for 3781 Updated!";
