c49b01ccc56c345a79b9c489022483e5339a1dc3
irhamfrdz <irhamfrdz@gmail.com>
2026-08-10 11:25:27 +0700

    Restore TailwindCSS script tag

--- resources/views/supir/checkpoint-create.blade.php
+++ resources/views/supir/checkpoint-create.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Lapor Lokasi & Foto Bukti - AYPSIS</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
