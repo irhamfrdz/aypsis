dc0852e64b5efd469d5bc596c5058dcf673bf4a5
Developer <dev@aypsis.test>
2026-08-26 09:55:04 +0700

    fix: tingkatkan z-index modal rekap absen agar menutupi navbar

--- resources/views/hrd-dashboard/index.blade.php
+++ resources/views/hrd-dashboard/index.blade.php
@@ -333,7 +333,7 @@
 </div>
 
 <!-- Modal Export Rekap Absen -->
-<div id="exportModal" class="fixed z-10 inset-0 overflow-y-auto hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+<div id="exportModal" class="fixed z-50 inset-0 overflow-y-auto hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true">
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true" onclick="closeExportModal()"></div>
 
