d9d3dd05ebd9d4981ed2b03856f3fc02a277f1f8
Aditkaya <adityakaya500@gmail.com>
2026-08-26 16:50:49 +0700

    Fix Luar Radius table width on smaller screens

--- resources/views/hrd-dashboard/index.blade.php
+++ resources/views/hrd-dashboard/index.blade.php
@@ -332,7 +332,7 @@
         </div>
         
         <!-- Tabel Absen Luar Radius -->
-        <div class="bg-white rounded-lg shadow-sm border border-gray-200 overflow-hidden flex flex-col h-full xl:col-span-4">
+        <div class="bg-white rounded-lg shadow-sm border border-gray-200 overflow-hidden flex flex-col h-full col-span-1 lg:col-span-2 xl:col-span-4">
             <div class="bg-red-50 border-b border-red-100 px-4 py-3 flex justify-between items-center">
                 <h3 class="font-semibold text-red-800 flex items-center">
                     <i class="fas fa-map-marker-alt mr-2 text-red-600"></i> Karyawan Absen Luar Radius
