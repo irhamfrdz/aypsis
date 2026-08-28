5aadf5d9a4a21c38f3b1e36f74d70a8af0adbe3c
Aditkaya <adityakaya500@gmail.com>
2026-08-28 13:23:12 +0700

    fix: display validation errors in user edit form and prevent password autofill

--- resources/views/master-user/edit.blade.php
+++ resources/views/master-user/edit.blade.php
@@ -303,6 +303,11 @@
     @if (isset($errors) && is_object($errors) && method_exists($errors, 'any') && $errors->any())
         <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-md mb-6">
             <strong class="font-bold">Oops! Terjadi kesalahan:</strong>
+            <ul class="list-disc pl-5 mt-1">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
     @endif
 
@@ -317,7 +322,7 @@
             </div>
             <div>
                 <label class="block text-sm font-medium text-gray-700">Password (kosongkan jika tidak ingin mengubah)</label>
-                <input type="password" name="password" class="mt-1 block w-full rounded-md border-gray-300 bg-gray-100 p-2.5" />
+                <input type="password" name="password" autocomplete="new-password" class="mt-1 block w-full rounded-md border-gray-300 bg-gray-100 p-2.5" />
             </div>
             <div>
                 <label class="block text-sm font-medium text-gray-700">Konfirmasi Password</label>
